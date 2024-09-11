19d641d801060f5f8cd2597ad89d8148fcf4a09f
Gouri <123@gmail.com>
2024-09-11 22:41:01 +0530

    adding the file

--- task.sh
+++ task.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p "enter the username:" username
 read -p "enter the password:" password
-if [ "$username"=="admin" ] && [ "$password"=="abcd123" ];
+if [ "$username" == "admin" ] && [ "$password" == "abcd123" ];
 then
 	echo "Authentication is successful"
 else
